89bcdb7c00805dc36cf7063b635bca79466fa358
zidni0192 <izedeledby@gmail.com>
2019-06-19 10:03:18 +0700

    renaming index.js file to day1.js

---
+++ day1.js
@@ -0,0 +1,34 @@
+let arrayOfObject = [
+    {
+        name : "Ahmad",
+        bod: "9 mei"
+    },
+    {
+        name : "Rusa",
+        bod : " 10 April"
+    },
+    {
+        name : "arie",
+        bod : "40 agustus"
+    }
+]
+let mapArray = arrayOfObject.map(mapItem =>mapItem)
+console.log(mapArray)
+console.log("\n")
+
+let objNew = {
+    name:"ayam",
+    bod:"350 september"
+}
+
+arrayOfObject.push(objNew)
+console.log(arrayOfObject)
+console.log("\n")
+
+arrayOfObject.splice(2,1,)
+console.log(arrayOfObject )
+console.log("\n")
+
+arrayOfObject[2].name= "Gorengan"
+console.log(arrayOfObject)
+console.log("\n")
